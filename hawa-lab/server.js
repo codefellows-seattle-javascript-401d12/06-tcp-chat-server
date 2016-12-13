1e8df6a8ed0344480afbb68cb29a51d0bd1d3938
Hawa Abdi <abdih17@hotmail.com>
2016-12-13 12:07:12 -0800

    Finished with error, close, and @nickname commands. Also finished documentation on the README.md file to help other developers understand the functionalities of each command, and how to use the app.

--- hawa-lab/server.js
+++ hawa-lab/server.js
@@ -25,6 +25,13 @@ ee.on('@dm', function(client, string) {
   });
 });
 
+//@nickname should allow a user change their nickname
+//change previous name in array?
+ee.on('@nickname', function(client, data) {
+  client.nickname = data.trim();
+  client.socket.write(`The clients nickname is now ${client.nickname}\n`);
+});
+
 ee.on('@all', function(client, string) {
   pool.forEach( c => {
     c.socket.write(`${client.nickname}: ${string}`);
@@ -41,8 +48,22 @@ server.on('connection', function(socket) {
   var client = new Client(socket);
   // pushing new clients into empty pool array
   pool.push(client);
-
   console.log('client', client.userID);
+
+  socket.on('error', (err) => {
+    console.log('You have an error.\n');
+  });
+
+  // When a socket emits the close event, the socket should be removed from the client pool
+  socket.on('close', function() {
+    pool.forEach(function(currentClient) {
+      if (client.userID === currentClient.userID) {
+        pool.splice(pool.indexOf(client), 1);
+      } else {
+        currentClient.socket.write(`${client.nickname} is disconnected from server.\n`);
+      }
+    });
+  });
 
   socket.on('data', function(data) {
     // data initially comes as a buffer, but we will convert into readable data.
@@ -58,5 +79,5 @@ server.on('connection', function(socket) {
 });
 
 server.listen(PORT, function() {
-  console.log(`Server up: ${PORT}`);
+  console.log(`Server up on: ${PORT}`);
 });
