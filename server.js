164ffdb075bc63d65f60c7acd5bca5b07120552c
kcirekcom <erick.f.mock@gmail.com>
2016-12-13 11:38:19 -0800

    started building out server.js/added global variables

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const net = require('net');
+const EE = require('events');
+const Client = require('./model/client.js');
+const PORT = process.env.PORT || 3000;
+const server = net.createServer();
+const ee = new EE();
+
+const pool = [];
