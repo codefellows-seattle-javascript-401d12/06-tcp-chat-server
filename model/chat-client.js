01b9963ef68326010a94a5171114cc97bf3d0fc9
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-13 11:28:13 -0800

    set up chat client object constructor

--- model/chat-client.js
+++ model/chat-client.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const uuid = require('node-uuid');
+
+const Client = module.exports = function(socekt) {
+  this.socket = socket;
+  this.handle = `user_${Math.random()}`;
+  this.id = uuid.v4();
+};
