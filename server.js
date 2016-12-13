03f80faecc4ef3e37d822ef276c04645d7c1f39a
kcirekcom <erick.f.mock@gmail.com>
2016-12-13 12:08:52 -0800

    @dm command is functioning properly

--- server.js
+++ server.js
@@ -9,12 +9,43 @@ const ee = new EE();
 
 const pool = [];
 
+ee.on('@dm', function(client, string) {
+  let nickname = string.split(' ').shift().trim();
+  let message = string.split(' ').slice(1).join(' ').trim();
+
+  console.log('nickname:', nickname);
+  console.log('message:', message);
+
+  //c = client
+  pool.forEach( c => {
+    if (c.nickname === nickname) {
+      c.socket.write(`${client.nickname}: ${message}`);
+    }
+  });
+});
+
+//display if @ is not used when writing a message
+ee.on('default', function(client, string) {
+  client.socket.write('not a command\n');
+});
+
 server.on('connection', function(socket) {
   var client = new Client(socket);
   pool.push(client);
 
-  console.log('client:', client.id);
   console.log('we have connected successfully');
+  console.log('client:', client.id);
+
+  socket.on('data', function(data) {
+    const command = data.toString().split(' ').shift().trim();
+
+    if (command.startsWith('@')) {
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+
+    ee.emit('default', client, data.toString());
+  });
 });
 
 server.listen(PORT, function() {
