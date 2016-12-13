7d8fff7163677560cba9263aabd3f012bd5126ce
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-12 20:01:58 -0800

    client object set up set up client object and include event to update nickname

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const ee = new EE();
 const Client = require('./model/client.js');
 const PORT = process.env.PORT || 8080;
 const server = net.createServer();
+const activeUsers = [];
 
 server.listen(PORT, () => {
   console.log(`Client connected on port ${PORT}`);
@@ -13,4 +14,32 @@ server.listen(PORT, () => {
 
 server.on('error', err => {
   throw err;
+});
+
+// server.close(console.log('Client has closed connection'));
+
+ee.on('userConnection', () => {
+  server.getConnections(function(err, count) {
+    if(err) throw err;
+    console.log(`Number of users online ${count}`);
+  });
+});
+
+ee.on('@nickname', function(client, string) {
+  client.nickname = string.split(' ');
+  client.socket.write(`Your new nickname is ${client.nickname}`);
+});
+
+server.on('connection', (socket) => {
+  let client = new Client(socket);
+  activeUsers.push(client.uuidv4);
+  ee.emit('userConnection');
+
+  socket.on('data', function(data) {
+    const command = data.toString('utf8').split(' ').shift().trim();
+    if(command.startsWith('@')) {
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+  });
 });
