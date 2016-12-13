9eca58da2535513b9a46995ab22188a9b98a19e3
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-13 11:27:27 -0800

    set up basic node server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const net = require('net');
+const EE = require('events');
+const Client = require('./model/chat-client.js');
+const PORT = process.env.PORT || 8000;
+const server = net.createServer();
+const ee = new EE();
+
+const chatters = [];
+
+server.listen(PORT, function() {
+  console.log(`Server running on port ${PORT}.`);
+});
