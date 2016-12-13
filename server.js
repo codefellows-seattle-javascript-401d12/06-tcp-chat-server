87337a4a85a8eaf46bf9273186562de1379f7738
kcirekcom <erick.f.mock@gmail.com>
2016-12-13 12:49:54 -0800

    close and error events emit, client can change nickname

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ ee.on('@dm', function(client, string) {
   let nickname = string.split(' ').shift().trim();
   let message = string.split(' ').slice(1).join(' ').trim();
 
-  console.log('nickname:', nickname);
-  console.log('message:', message);
+  // console.log('nickname:', nickname);
+  // console.log('message:', message);
 
   //c = client
   pool.forEach( c => {
@@ -22,6 +22,19 @@ ee.on('@dm', function(client, string) {
       c.socket.write(`${client.nickname}: ${message}`);
     }
   });
+});
+
+//send a message to all
+ee.on('@all', function(client, string) {
+  pool.forEach( c => {
+    c.socket.write(`${client.nickname}: ${string}`);
+  });
+});
+
+//client can change their nickname, new nickname will broadcast to all
+ee.on('@nickname', function(client, string) {
+  client.nickname = string.trim();
+  client.socket.write(`${string} is your new nickname`);
 });
 
 //display if @ is not used when writing a message
@@ -45,6 +58,17 @@ server.on('connection', function(socket) {
     }
 
     ee.emit('default', client, data.toString());
+  });
+
+  socket.on('error', function(err) {
+    console.log(err);
+  });
+
+  //this will show that client is disconnected when closed
+  socket.on('close', function() {
+    pool.forEach(function(client) {
+      client.socket.write(`${client.nickname} is disconnected`);
+    });
   });
 });
 
