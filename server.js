f5ca06934ee74174d718c9bc1e8215154b2397bc
kcirekcom <erick.f.mock@gmail.com>
2016-12-13 11:46:05 -0800

    connected successfully

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const ee = new EE();
 
 const pool = [];
 
+server.on('connection', function(socket) {
+  var client = new Client(socket);
+  console.log('we have connected successfully'); 
+});
+
 server.listen(PORT, function() {
   console.log(`server up: ${PORT}`);
 });
