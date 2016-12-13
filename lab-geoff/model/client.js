0a24966d8805ec74dddef63a86be4b606037a42a
Geoff Simons <geoff.simons@gmail.com>
2016-12-13 04:33:49 -0800

    Nicer random user number

--- lab-geoff/model/client.js
+++ lab-geoff/model/client.js
@@ -4,7 +4,7 @@ const uuid = require('node-uuid');
 
 const Client = module.exports = function(socket) {
   this.socket = socket;
-  this.nickname = `user_${Math.random()}`;
+  this.nickname = `user_${Math.round(10000*Math.random())}`;
   this.id = uuid.v4();
 };
 
