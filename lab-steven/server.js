dd4ef67690c22e1e572ac8fb4ba4556faf3eb1d3
Steven Bateman <batemanvo@gmail.com>
2016-12-12 18:39:32 -0800

    Started work on server.js. Created server.listen function and started writing server.on('connection') function.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const net = require('net');
+const EE = require('events');
+const Client = require('./model/client-constructor.js');
+const PORT = process.env.PORT || 8080;
+const server = net.createServer();
+const ee = new EE();
+
+const connectedClients = [];
+
+server.on('connection', function(socket) {
+  const client = new Client(socket);
+  connectedClients.push(client);
+});
+
+server.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}.`);
+});
