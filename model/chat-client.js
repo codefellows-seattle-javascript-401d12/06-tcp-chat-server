4386410666c98f63ea9eede42b158f4061890679
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-14 11:06:27 -0800

    changed constructor param socekt to socket. derrrrrp.

--- model/chat-client.js
+++ model/chat-client.js
@@ -2,7 +2,7 @@
 
 const uuid = require('node-uuid');
 
-const Client = module.exports = function(socekt) {
+const Client = module.exports = function(socket) {
   this.socket = socket;
   this.handle = `user_${Math.random()}`;
   this.id = uuid.v4();
