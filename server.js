89b7ea61ade4a79ee56114e69fc0db1a95be3bba
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-14 11:02:10 -0800

    build out direct message capability from lecture demo

--- server.js
+++ server.js
@@ -7,6 +7,39 @@ const PORT = process.env.PORT || 8000;
 const server = net.createServer();
 const ee = new EE();
 
+const chatters = [];
+
+ee.on('@dm', function(client, input) {
+  console.log('@dm logged');
+  let handle = input.split(' ').shift().trim();
+  let directMessage = input.split(' ').slice(1).join(' ').trim();
+
+  console.log('handle:', handle);
+  console.log('message:', directMessage);
+
+  chatters.forEach( chatter => {
+    if (chatter.handle === handle) {
+      console.log('data written');
+      chatter.socket.write(`${client.handle}: ${directMessage}`);
+    }
+  });
+});
+
+server.on('connection', function(socket) {
+  var chatClient = new Client(socket);
+
+  socket.on('data', function(data) {
+    const befehl = data.toString().split(' ').shift().trim();
+
+    console.log(befehl);
+
+    if (befehl.startsWith('@')) {
+      ee.emit(befehl, chatClient, data.toString().split(' ').slice(1).join(' '));
+      console.log('ee emitted');
+    }
+  });
+});
+
 server.listen(PORT, function() {
   console.log(`Server running on port ${PORT}.`);
 });
