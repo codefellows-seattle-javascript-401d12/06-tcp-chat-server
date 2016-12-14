23e83d226679c0e4174f19e4f656909c51920a22
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-14 11:13:32 -0800

    add default command event

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ ee.on('@dm', function(client, input) {
   });
 });
 
+ee.on('default', function(client, input) {
+  client.socket.write('keine Befehl gegeben\n');
+});
+
 server.on('connection', function(socket) {
   var chatClient = new Client(socket);
 
@@ -36,7 +40,9 @@ server.on('connection', function(socket) {
     if (befehl.startsWith('@')) {
       ee.emit(befehl, chatClient, data.toString().split(' ').slice(1).join(' '));
       console.log('ee emitted');
+      return;
     }
+    ee.emit('default', chatClient, data.toString());
   });
 });
 
