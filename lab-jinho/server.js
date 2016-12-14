462d2a35652051a6854f970a2daf287496c0f8eb
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-13 16:00:54 -0800

    added server socket

--- lab-jinho/server.js
+++ lab-jinho/server.js
@@ -6,3 +6,28 @@ const Client = require('./model/client.js');
 const PORT = process.env.PORT || 3000;
 const server = net.createServer();
 const ee = new EE();
+
+const pool = [];
+
+
+//server socket
+server.on('connection', function(socket) {
+  var client = new Client(socket);
+  pool.push(client);
+
+  socket.on('data', function(data) {
+    const command = data.toString().split(' ').shift().trim();
+    console.log('command:', command);
+
+    if (command.startsWith('@')) {
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+
+    ee.emit('default', client, data.toString());
+  });
+});
+
+server.listen(PORT, function() {
+  console.log('server up:', PORT);
+});
